aee860543661cfb4fbe56674747bd42dedfdd24b
nur <nurlan.mannapbaev@gmail.com>
2022-03-09 21:27:30 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+  Ychburchtuk ychburchtuk = new Ychburchtuk();
+        ychburchtuk.a = 3;
+        ychburchtuk.b = 4;
+        ychburchtuk.c = 5;
+
+        System.out.println( ychburchtuk.area());
+
+
+
+            }
+}
---
+++ src/com/company/Ychburchtuk.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Ychburchtuk {
+    int a;
+    int b;
+    int c;
+
+    int area(){
+        return ((a*b)/2);
+    }
+
+}
